96fcb0ce8a7b6f9cbf07142ee7192ab94b6f0b29
BobaTheBuilder <ivanmorozko97@gmail.com>
2023-05-16 23:03:18 +0300

    Fixed Reg page

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -10,6 +10,7 @@ import down from "../assets/hero-bg-down.svg";
 import FloatingLabel from "react-bootstrap/cjs/FloatingLabel";
 
 const Registration = () => {
+<<<<<<< Updated upstream
     return (
         <Container fluid className="background px-0 overflow-auto">
             <Container fluid className="d-flex flex-column justify-content-between h-100 px-0">
@@ -24,6 +25,95 @@ const Registration = () => {
                     <Form className="reg-container d-flex flex-column justify-content-around">
                         <div className="reg-text text-center">Регистрация</div>
                         <Container>
+=======
+    const [regData, setRegData] = useState(null);
+
+    function handleInputsChange(key, value) {
+        setRegData({...regData, [key]: value})
+        console.log(regData);
+    }
+
+    function handleClick() {
+        try {
+            console.log(Object.keys(regData).length);
+        }
+        catch (e) {
+            console.log(e.value);
+        }
+
+    }
+
+    return (
+        <Container fluid className="justify-content-center mt-5">
+            <Form className="reg-container d-flex flex-column justify-content-around mx-auto">
+                <div className="reg-text text-center">Регистрация</div>
+                <Container>
+                    <Form.Group className="mb-1" controlId="name">
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Имя"
+                        >
+                            <Form.Control className="reg-input" type="text" placeholder="Имя"
+                                          onChange={(e) => handleInputsChange("name", e.target.value)}/>
+                        </FloatingLabel>
+                    </Form.Group>
+                    <Form.Group className="mb-1" controlId="surname">
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Фамилия"
+                        >
+                            <Form.Control className="reg-input" type="text" placeholder="Фамилия"
+                                          onChange={(e) => handleInputsChange("surname", e.target.value)}/>
+                        </FloatingLabel>
+                    </Form.Group>
+                    <Form.Group className="mb-1" controlId="patronymic">
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Отчество"
+                        >
+                            <Form.Control className="reg-input" type="text" placeholder="Отчество"
+                                          onChange={(e) => handleInputsChange("patronymic", e.target.value)}/>
+                        </FloatingLabel>
+                    </Form.Group>
+                    <Form.Group className="mb-1" controlId="login">
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Придумайте логин"
+                        >
+                            <Form.Control className="reg-input" type="text" placeholder="Придумайте логин"
+                                          onChange={(e) => handleInputsChange("login", e.target.value)}/>
+                        </FloatingLabel>
+                    </Form.Group>
+                    <Form.Group className="mb-1" controlId="password">
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Придумайте пароль"
+                        >
+                            <Form.Control className="reg-input" type="password"
+                                          placeholder="Придумайте пароль"
+                                          onChange={(e) => handleInputsChange("password", e.target.value)}/>
+                        </FloatingLabel>
+                    </Form.Group>
+                    <Form.Group className="mb-1" controlId="rep-password">
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Повторите пароль"
+                        >
+                            <Form.Control className="reg-input" type="password" placeholder="Повторите пароль"
+                                          onChange={(e) => handleInputsChange("rep-password", e.target.value)}/>
+                        </FloatingLabel>
+                    </Form.Group>
+                    <Form.Group className="mb-4" controlId="phone">
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Номер мобильного телефона"
+                        >
+                            <Form.Control className="reg-input" type="phone"
+                                          placeholder="Номер мобильного телефона"
+                                          onChange={(e) => handleInputsChange("phone", e.target.value)}/>
+                        </FloatingLabel>
+                    </Form.Group>
+>>>>>>> Stashed changes
 
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <FloatingLabel
