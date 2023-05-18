981408a2b0859744f88618bb247fc780ae616f06
Morozilka <ivanmorozko97@gmail.com>
2023-05-18 23:09:35 +0300

    Redesigned pages using MUI.

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
-import {PaletteTree} from './palette'
-import App from "../App";
+import { Previews } from '@react-buddy/ide-toolbox'
+import { PaletteTree } from './palette'
 
 const ComponentPreviews = () => {
     return (
-        <Previews palette={<PaletteTree/>}>
-            <ComponentPreview path="/App">
-                <App/>
-            </ComponentPreview>
+        <Previews palette={<PaletteTree />}>
         </Previews>
     )
 }
